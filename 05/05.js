bee4236971bb437e8e2cf5e71f60ae3c749426dd
St4rJ1n <ojryu@pusan.ac.kr>
2024-04-26 16:05:12 +0900

    업다운 코드 완성, 팝업 수정

--- 05/05.js
+++ 05/05.js
@@ -14,33 +14,78 @@
         - 초기화 : 랜덤 수 새로 생성, input 박스 보이기..
 */
 
+//화면 이미지 
+const showImg = (imgName) => {
+    const wht = document.querySelector('#wht') ;
+    wht.setAttribute('src', `./img/${imgName}.png`) ;
+    wht.setAttribute('alt', `${imgName}`) ;
+  }
+
+
 document.addEventListener('DOMContentLoaded',()=>{
     // 1. DOM에서 제어할 노드를 가져오기
-    const wht = document.querySelector('#wht') ;
     const input = document.querySelector('#txt1') ;
     const bt = document.querySelector('#bt') ;
 
     //랜덤 수
     let n ;
     let flag = false ;
+    let imgName ;
+
+    alert('값을 입력하세요.');
+    input.focus();
 
     bt.addEventListener('click', (e)=>{
         //form 태그 사용시 다시 호출되지 않도록
         e.preventDefault();
+
         //랜덤 수 생성 : flag가 true일 때만 생성
         if (!flag) {
             flag = true
             n = Math.floor(Math.random()*100) + 1 ; //1~100
-            console.log('n=', n) ;
+            console.log('n=', n, flag)
+            showImg('what');
+            input.computedStyleMap.display = 'inline';
+            bt.textContent = '확인';
+
         }
 
         //입력값 체크
         if (input.value == '') {
-            alert('값을 입력하세요.')
+            alert('값을 입력하세요.');
             input.focus();
             return ;
         }
 
-        console.log(n);
-    });
-});
+        //입력값 체크 : 1 ~ 100
+        const user = parseInt(input.value) ;
+        console.log(user)
+        if ( user < 1 || user > 100) {
+        alert('[입력오류] 1~100사이의 숫자를 입력하세요.');
+        input.focus();
+        return ;
+        }
+
+        // input 박스 값이 랜덤 수 보다 작으면 up 이미지
+        // input 박스 값이 랜덤 수 보다 크면 down 이미지
+        // input 박스 값이 랜덤 수와 같으면 good 이미지
+
+        if (user < n) imgName = 'up';
+        else if (user > n) imgName = 'down';
+        else {
+            imgName = 'good';
+
+            input.computedStyleMap.display ='none' ;
+            bt.textContent = '숫자를 다시 생성해 주세요.';
+            flag = false ;
+        }
+
+
+        if (imgName === 'up' || imgName === 'down') {
+            input.focus();
+          }
+          
+          input.value = '';
+          showImg(imgName) ;
+        });
+      });
